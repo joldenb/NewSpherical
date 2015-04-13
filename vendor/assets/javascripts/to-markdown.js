1fb556a52fd29f00575915e359502eb2b65b68b8
Victor Grey <victor@planetwork.net>
2015-04-13 08:02:33 -0700

    Reverted changes in to-markdown.js

--- vendor/assets/javascripts/to-markdown.js
+++ vendor/assets/javascripts/to-markdown.js
@@ -1,4 +1,4 @@
-;(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this};g.toMarkdown = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.toMarkdown = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
  * to-markdown - an HTML to Markdown converter
  *
@@ -400,11 +400,11 @@ exports.isBlockLevel = function (node) {
 		'\'': '&#x27;',
 		'<': '&lt;',
 		// See http://mathiasbynens.be/notes/ambiguous-ampersands: in HTML, the
-		// following is not strictly necessary unless its part of a tag or an
-		// unquoted attribute value. Were only escaping it to support those
+		// following is not strictly necessary unless it’s part of a tag or an
+		// unquoted attribute value. We’re only escaping it to support those
 		// situations, and for XML support.
 		'>': '&gt;',
-		// In Internet Explorer  8, the backtick character can be used
+		// In Internet Explorer ≤ 8, the backtick character can be used
 		// to break out of (un)quoted attribute values or HTML comments.
 		// See http://html5sec.org/#102, http://html5sec.org/#108, and
 		// http://html5sec.org/#133.
@@ -459,9 +459,9 @@ exports.isBlockLevel = function (node) {
 		var output = '';
 		if ((codePoint >= 0xD800 && codePoint <= 0xDFFF) || codePoint > 0x10FFFF) {
 			// See issue #4:
-			// Otherwise, if the number is in the range 0xD800 to 0xDFFF or is
+			// “Otherwise, if the number is in the range 0xD800 to 0xDFFF or is
 			// greater than 0x10FFFF, then this is a parse error. Return a U+FFFD
-			// REPLACEMENT CHARACTER.
+			// REPLACEMENT CHARACTER.”
 			if (strict) {
 				parseError('character reference outside the permissible Unicode range');
 			}
@@ -546,7 +546,7 @@ exports.isBlockLevel = function (node) {
 				return '&' + encodeMap[string] + ';';
 			});
 		} else {
-			// Encode `<>"'&` using hexadecimal escapes, now that theyre not handled
+			// Encode `<>"'&` using hexadecimal escapes, now that they’re not handled
 			// using named character references.
 			string = string.replace(regexEscape, hexEscape);
 		}
@@ -616,7 +616,7 @@ exports.isBlockLevel = function (node) {
 					return $0;
 				}
 			}
-			// If were still here, its a legacy reference for sure. No need for an
+			// If we’re still here, it’s a legacy reference for sure. No need for an
 			// extra `if` check.
 			// Decode named character references without trailing `;`, e.g. `&amp`
 			// This is only a parse error if it gets converted to `&`, or if it is
