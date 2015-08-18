3c099946ee6cb7badd2484133a99863b129632ab
Joe Oldenburg <joseph.oldenburg@gmail.com>
2015-08-18 14:38:29 -0600

    initial commit with yeoman, angular, boilerplate, gulp

---
+++ client/gulpfile.js
@@ -0,0 +1,40 @@
+/**
+ *  Welcome to your gulpfile!
+ *  The gulp tasks are splitted in several files in the gulp directory
+ *  because putting all here was really too long
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var wrench = require('wrench');
+var requireDir = require('require-dir');
+var gulpDir = requireDir('./gulp');
+var server = require('./gulp/server.js');
+server({tmp:'.'});
+
+var taskListing = require('gulp-task-listing');
+ 
+// Add a task to render the output 
+gulp.task('help', taskListing);
+ 
+/**
+ *  This will load all js or coffee files in the gulp directory
+ *  in order to load all gulp tasks
+ */
+wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function(file) {
+  require('./gulp/' + file);
+});
+
+
+/**
+ *  Default task clean temporaries directories and launch the
+ *  main optimization build task
+ */
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
+
+
