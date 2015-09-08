d232bcaf7c5c952ce22ee17504f79372ccbbc7f9
Joe Oldenburg <joseph.oldenburg@gmail.com>
2015-09-08 11:32:30 -0600

    adding more background toggles

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -8,6 +8,9 @@
   /** @ngInject */
   function MainController($scope, $http, $timeout, webDevTec, toastr) {
     var vm = this;
+    $scope.backgroundcolor = 'white';
+    $scope.backgroundimage= 'full';
+    $scope.numberOfRows = 2;
 
     $http.get('http://v3.api.r88r.net/v3/headlines?cname=V3A.electricvehicles').
     success(function(data, status, headers, config) {
@@ -45,5 +48,86 @@
         awesomeThing.rank = Math.random();
       });
     } 
+
+    $scope.toggleBackgroundColor = function() {
+        switch($scope.backgroundcolor) {
+            case 'white':
+                $scope.backgroundcolor = 'gray';
+                break;
+            case 'gray':
+                $scope.backgroundcolor = 'black';
+                break;
+            case 'black':
+                $scope.backgroundcolor = 'white';
+                break;
+        }
+    };//End of the Background Color Toggle
+
+    $scope.toggleBackgroundOpacity = function() {
+        switch($scope.backgroundimage) {
+            case 'clear':
+                $scope.backgroundimage = 'full';
+                break;
+            case 'full':
+                $scope.backgroundimage = 'half';
+                break;
+            case 'half':
+                $scope.backgroundimage = 'clear';
+                break;
+        }
+    };//End of the Background Image Toggle
+
+    $scope.toggleNumberOfRows = function() {
+        switch($scope.numberOfRows) {
+            case 1:
+                $scope.numberOfRows = 2;
+                break;
+            case 2:
+                $scope.numberOfRows = 3;
+                break;
+            case 3:
+                $scope.numberOfRows = 1;
+                break;
+        }        
+    };
+
+    $scope.newColumn = function(index) {
+        if ( index % $scope.numberOfRows == 0 ) { 
+            return true; 
+        } else {
+            return false;
+        }
+    };
+
+
+    $scope.atLeastTwoRows = function() {
+        if ( $scope.numberOfRows == 2 || $scope.numberOfRows == 3 ){
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    $scope.atLeastThreeRows = function() {
+        if ( $scope.numberOfRows == 3 ){
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 })();
