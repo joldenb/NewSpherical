b249a825aefa1b953c50a8e20a912f62d5926f01
Joe Oldenburg <joseph.oldenburg@gmail.com>
2015-09-14 10:51:15 -0600

    add scrolling up

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -8,7 +8,33 @@
   /** @ngInject */
   function MainController($scope, $http, $timeout, webDevTec, toastr) {
     var vm = this;
-    $scope.backgroundcolor = 'white';
+        $(".sphere-top-img-static").attr("src", "/assets/images/forest-picture-circle.png");
+        $(".sphere-top-img-static").css("height","400px");
+        $(".sphere-top-img-static").css("width","400px");
+        $(".sphere-top-img-static").css("position","absolute");
+        $(".sphere-top-img-static").css("top", "-250px");
+        $(".sphere-top-img-static").css("left","35%");
+        $(".sphere-top-img-static").css("z-index","1");
+        $(".sphere-top-img-static").css("opacity",".5");
+
+        $(".sphere-middle-img-static").attr("src", "/assets/images/cryo_sphere1.png");
+        $(".sphere-middle-img-static").css("height","500px");
+        $(".sphere-middle-img-static").css("width","500px");
+        $(".sphere-middle-img-static").css("position","absolute");
+        $(".sphere-middle-img-static").css("top", "20%");
+        $(".sphere-middle-img-static").css("left","31%");        
+        $(".sphere-middle-img-static").css("z-index","1");
+
+        $(".sphere-bottom-img-static").attr("src", "/assets/images/forest-picture-circle.png");
+        $(".sphere-bottom-img-static").css("height","400px");
+        $(".sphere-bottom-img-static").css("width","400px");
+        $(".sphere-bottom-img-static").css("position","absolute");
+        $(".sphere-bottom-img-static").css("top", "85%");
+        $(".sphere-bottom-img-static").css("left","35%");        
+        $(".sphere-bottom-img-static").css("z-index","1");
+        $(".sphere-bottom-img-static").css("opacity",".5");
+
+$scope.backgroundcolor = 'white';
     $scope.backgroundimage= 'full';
     $scope.numberOfRows = 2;
 
@@ -66,12 +92,15 @@
     $scope.toggleBackgroundOpacity = function() {
         switch($scope.backgroundimage) {
             case 'clear':
+                $(".sphere-middle-img-static").css("opacity","1");
                 $scope.backgroundimage = 'full';
                 break;
             case 'full':
+                $(".sphere-middle-img-static").css("opacity",".5");;
                 $scope.backgroundimage = 'half';
                 break;
             case 'half':
+                $(".sphere-middle-img-static").css("opacity","0");
                 $scope.backgroundimage = 'clear';
                 break;
         }
@@ -116,8 +145,92 @@
         }
     };
 
+    $scope.scrollUpSphere = function() {
+        $(".scrollBarWrapper").fadeOut(400);
+        $(".sphere-top-img-static").fadeOut(400);
+        $(".sphere-middle-img-static").fadeOut(400);
+        $(".sphere-bottom-img-static").fadeOut(400);
+
+        $scope.topSphereUrl = $(".sphere-top-img-static").attr("src");
+        $scope.middleSphereUrl = $(".sphere-middle-img-static").attr("src");
+        $scope.bottomSphereUrl = $(".sphere-bottom-img-static").attr("src");
+
+        $(".sphere-top-img").attr("src", $scope.topSphereUrl);
+        $(".sphere-top-img").css("height","400px");
+        $(".sphere-top-img").css("width","400px");
+        $(".sphere-top-img").css("position","absolute");
+        $(".sphere-top-img").css("top", "-250px");
+        $(".sphere-top-img").css("left","35%");
+        $(".sphere-top-img").css("z-index","3");
+
+        $(".sphere-middle-img").attr("src", $scope.middleSphereUrl);
+        $(".sphere-middle-img").css("height","500px");
+        $(".sphere-middle-img").css("width","500px");
+        $(".sphere-middle-img").css("position","absolute");
+        $(".sphere-middle-img").css("top", "20%");
+        $(".sphere-middle-img").css("left","31%");        
+        $(".sphere-middle-img").css("z-index","3");
+
+        $(".sphere-bottom-img").attr("src", $scope.topSphereUrl);
+        $(".sphere-bottom-img").css("height","400px");
+        $(".sphere-bottom-img").css("width","400px");
+        $(".sphere-bottom-img").css("position","absolute");
+        $(".sphere-bottom-img").css("top", "85%");
+        $(".sphere-bottom-img").css("left","35%");        
+        $(".sphere-bottom-img").css("z-index","3");
+
+        $(".sphere-top-img").fadeIn(400);
+        $(".sphere-middle-img").fadeIn(400);
+        $(".sphere-bottom-img").fadeIn(400);
 
 
+        $(".sphere-top-img").animate({
+                top: '20%'
+            }, 1000, function(){
+                //$(".sphere-top").toggle();
+        });
+       
+        $(".sphere-middle-img").animate({
+            top: '85%',
+        }, 1000, function(){
+
+                $scope.newMiddleSphere = $(".sphere-top-img").attr("src");
+                $scope.newBottomSphere = $(".sphere-middle-img").attr("src");
+                $(".sphere-middle-img-static").attr("src", $scope.newMiddleSphere);
+                $(".sphere-bottom-img-static").attr("src", $scope.newBottomSphere);
+                $(".sphere-top-img-static").attr("src", $scope.newBottomSphere);
+
+                $(".sphere-top-img").fadeOut(400);
+                $(".sphere-middle-img").fadeOut(400);
+                $(".sphere-bottom-img").fadeOut(400);
+
+                $(".scrollBarWrapper").fadeIn(400);
+                $(".sphere-top-img-static").fadeIn(400);
+                $(".sphere-middle-img-static").fadeIn(400);
+                $(".sphere-bottom-img-static").fadeIn(400);
+
+
+        });
+
+        $(".sphere-bottom-img").animate({
+            top: '150%'
+        }, 1000);
+    };
+
+
+$(window).bind('mousewheel', function(e){
+    if(e.originalEvent.wheelDelta > 200 && !$scope.scrollEventTriggered)
+    {
+        $scope.scrollUpSphere();
+        $scope.scrollEventTriggered = true;
+    } 
+    else if (e.originalEvent.wheelDelta < 200 && $scope.scrollEventTriggered)
+    {
+        
+        $scope.scrollEventTriggered = false;
+    }
+
+});
 
 
 
