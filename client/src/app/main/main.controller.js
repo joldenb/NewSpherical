1a05bf0a4cdc53b581981a785cd5d6e523879560
Joe Oldenburg <joseph.oldenburg@gmail.com>
2015-09-16 12:10:49 -0600

    adding downward scrolling

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -7,73 +7,32 @@
 
   /** @ngInject */
   function MainController($scope, $http, $timeout, webDevTec, toastr) {
-    var vm = this;
-        $(".sphere-top-img-static").attr("src", "/assets/images/forest-picture-circle.png");
-        $(".sphere-top-img-static").css("height","400px");
-        $(".sphere-top-img-static").css("width","400px");
-        $(".sphere-top-img-static").css("position","absolute");
-        $(".sphere-top-img-static").css("top", "-250px");
-        $(".sphere-top-img-static").css("left","35%");
-        $(".sphere-top-img-static").css("z-index","1");
-        $(".sphere-top-img-static").css("opacity",".5");
+    var preLoadedSpheres = 
+    [
+        "/assets/images/cryo_sphere1.png",
+        "/assets/images/atmosphere.png",
+        "/assets/images/forest-picture-circle.png"
+    ]
 
-        $(".sphere-middle-img-static").attr("src", "/assets/images/cryo_sphere1.png");
-        $(".sphere-middle-img-static").css("height","500px");
-        $(".sphere-middle-img-static").css("width","500px");
-        $(".sphere-middle-img-static").css("position","absolute");
-        $(".sphere-middle-img-static").css("top", "20%");
-        $(".sphere-middle-img-static").css("left","31%");        
-        $(".sphere-middle-img-static").css("z-index","1");
+    var currentSphereIndex = 0;
 
-        $(".sphere-bottom-img-static").attr("src", "/assets/images/forest-picture-circle.png");
-        $(".sphere-bottom-img-static").css("height","400px");
-        $(".sphere-bottom-img-static").css("width","400px");
-        $(".sphere-bottom-img-static").css("position","absolute");
-        $(".sphere-bottom-img-static").css("top", "85%");
-        $(".sphere-bottom-img-static").css("left","35%");        
-        $(".sphere-bottom-img-static").css("z-index","1");
-        $(".sphere-bottom-img-static").css("opacity",".5");
+    $(".sphere-top-img-static").attr("src", preLoadedSpheres[currentSphereIndex + 1]);
+    $(".sphere-middle-img-static").attr("src", preLoadedSpheres[currentSphereIndex]);
+    $(".sphere-bottom-img-static").attr("src", preLoadedSpheres[preLoadedSpheres.length - 1]);
 
-$scope.backgroundcolor = 'white';
+    $scope.backgroundcolor = 'white';
     $scope.backgroundimage= 'full';
     $scope.numberOfRows = 2;
 
     $http.get('http://v3.api.r88r.net/v3/headlines?cname=V3A.electricvehicles').
-    success(function(data, status, headers, config) {
-        $scope.r88rResponse = data.data.headlines;
-        var stop = "stop";
-    }).
-    error(function(data, status, headers, config) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
+        success(function(data, status, headers, config) {
+            $scope.r88rResponse = data.data.headlines;
+            var stop = "stop";
+        }).
+        error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
     });
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1439942576538;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    } 
 
     $scope.toggleBackgroundColor = function() {
         switch($scope.backgroundcolor) {
@@ -145,53 +104,70 @@ $scope.backgroundcolor = 'white';
         }
     };
 
-    $scope.scrollUpSphere = function() {
-        $(".scrollBarWrapper").fadeOut(400);
-        $(".sphere-top-img-static").fadeOut(400);
-        $(".sphere-middle-img-static").fadeOut(400);
-        $(".sphere-bottom-img-static").fadeOut(400);
-
+    $scope.setAnimationSphereStyles = function(){
         $scope.topSphereUrl = $(".sphere-top-img-static").attr("src");
         $scope.middleSphereUrl = $(".sphere-middle-img-static").attr("src");
         $scope.bottomSphereUrl = $(".sphere-bottom-img-static").attr("src");
 
         $(".sphere-top-img").attr("src", $scope.topSphereUrl);
-        $(".sphere-top-img").css("height","400px");
-        $(".sphere-top-img").css("width","400px");
-        $(".sphere-top-img").css("position","absolute");
-        $(".sphere-top-img").css("top", "-250px");
-        $(".sphere-top-img").css("left","35%");
-        $(".sphere-top-img").css("z-index","3");
-
+        $(".sphere-top-img").css({
+          "height":"400px",  
+          "width":"400px",
+          "position":"absolute",
+          "top":"-40%",
+          "left":"50%",
+          "margin-left":"-200px",
+          "z-index":"5"
+        });
+        
         $(".sphere-middle-img").attr("src", $scope.middleSphereUrl);
-        $(".sphere-middle-img").css("height","500px");
-        $(".sphere-middle-img").css("width","500px");
-        $(".sphere-middle-img").css("position","absolute");
-        $(".sphere-middle-img").css("top", "20%");
-        $(".sphere-middle-img").css("left","31%");        
-        $(".sphere-middle-img").css("z-index","3");
+        $(".sphere-middle-img").css({
+          "height":"500px",  
+          "width":"500px",
+          "position":"absolute",
+          "top":"50%",
+          "left":"50%",
+          "margin-top":"-250px",
+          "margin-left":"-250px",
+          "z-index":"4"
+        });
+
 
         $(".sphere-bottom-img").attr("src", $scope.topSphereUrl);
-        $(".sphere-bottom-img").css("height","400px");
-        $(".sphere-bottom-img").css("width","400px");
-        $(".sphere-bottom-img").css("position","absolute");
-        $(".sphere-bottom-img").css("top", "85%");
-        $(".sphere-bottom-img").css("left","35%");        
-        $(".sphere-bottom-img").css("z-index","3");
+        $(".sphere-bottom-img").css({
+          "height":"400px",  
+          "width":"400px",
+          "position":"absolute",
+          "top":"90%",
+          "left":"50%",
+          "margin-left":"-200px",
+          "z-index":"3"
+        });
 
         $(".sphere-top-img").fadeIn(400);
         $(".sphere-middle-img").fadeIn(400);
         $(".sphere-bottom-img").fadeIn(400);
 
 
+    };
+
+
+    $scope.scrollUpSphere = function() {
+        $(".scrollBarWrapper").fadeOut(400);
+        $(".sphere-top-img-static").fadeOut(400);
+        $(".sphere-middle-img-static").fadeOut(400);
+        $(".sphere-bottom-img-static").fadeOut(400);
+
+        //This function will get all the animation spheres ready to go to 
+        //transition up a sphere.
+        $scope.setAnimationSphereStyles();
+
         $(".sphere-top-img").animate({
-                top: '20%'
-            }, 1000, function(){
-                //$(".sphere-top").toggle();
-        });
+                top: '30%'
+        }, 1000);
        
         $(".sphere-middle-img").animate({
-            top: '85%',
+            top: '100%',
         }, 1000, function(){
 
                 $scope.newMiddleSphere = $(".sphere-top-img").attr("src");
@@ -207,40 +183,84 @@ $scope.backgroundcolor = 'white';
                 $(".scrollBarWrapper").fadeIn(400);
                 $(".sphere-top-img-static").fadeIn(400);
                 $(".sphere-middle-img-static").fadeIn(400);
-                $(".sphere-bottom-img-static").fadeIn(400);
-
+                $(".sphere-bottom-img-static").fadeIn(400, function(){
+                    $scope.scrollEventTriggered = false;
+                });
 
         });
 
         $(".sphere-bottom-img").animate({
-            top: '150%'
+            top: '250%'
+        }, 1000);
+    };
+    $scope.scrollDownSphere = function() {
+        $(".scrollBarWrapper").fadeOut(400);
+        $(".sphere-top-img-static").fadeOut(400);
+        $(".sphere-middle-img-static").fadeOut(400);
+        $(".sphere-bottom-img-static").fadeOut(400);
+
+        //This function will get all the animation spheres ready to go to 
+        //transition up a sphere.
+        $scope.setAnimationSphereStyles();
+
+        $(".sphere-top-img").animate({
+                top: '-250%'
+        }, 1000);
+       
+        $(".sphere-middle-img").animate({
+            top: '-10%',
+        }, 1000, function(){
+
+                $scope.newMiddleSphere = $(".sphere-top-img").attr("src");
+                $scope.newBottomSphere = $(".sphere-middle-img").attr("src");
+                $(".sphere-middle-img-static").attr("src", $scope.newMiddleSphere);
+                $(".sphere-bottom-img-static").attr("src", $scope.newBottomSphere);
+                $(".sphere-top-img-static").attr("src", $scope.newBottomSphere);
+
+                $(".sphere-top-img").fadeOut(400);
+                $(".sphere-middle-img").fadeOut(400);
+                $(".sphere-bottom-img").fadeOut(400);
+
+                $(".scrollBarWrapper").fadeIn(400);
+                $(".sphere-top-img-static").fadeIn(400);
+                $(".sphere-middle-img-static").fadeIn(400);
+                $(".sphere-bottom-img-static").fadeIn(400, function(){
+                    $scope.scrollEventTriggered = false;
+                });
+
+        });
+
+        $(".sphere-bottom-img").animate({
+            top: '25%'
         }, 1000);
     };
 
 
-$(window).bind('mousewheel', function(e){
-    if(e.originalEvent.wheelDelta > 200 && !$scope.scrollEventTriggered)
-    {
-        $scope.scrollUpSphere();
-        $scope.scrollEventTriggered = true;
-    } 
-    else if (e.originalEvent.wheelDelta < 200 && $scope.scrollEventTriggered)
-    {
-        
-        $scope.scrollEventTriggered = false;
-    }
+    $(window).bind('wheel', function(e){
+        if(e.originalEvent.deltaY < -75 && !$scope.scrollEventTriggered)
+        {
+            $scope.scrollUpSphere();
+            $scope.scrollEventTriggered = true;
+        } else if (e.originalEvent.deltaY > 75 && !$scope.scrollEventTriggered)
+        {
+            $scope.scrollDownSphere();
+            $scope.scrollEventTriggered = true;
+        }
 
-});
-
-
-
-
-
-
-
-
-
-
+    });
 
   }
+
+    $(window).bind('onscroll', function(event) {
+        // jQuery clones events, but only with a limited number of properties for perf reasons. Need the original event to get 'touches'
+        var e = event.originalEvent;
+//        scrollStartPos = $(this).scrollTop() + e.touches[0].pageY;
+//        e.preventDefault();
+    });
+    $(window).bind('touchmove', function(event) {
+        var e = event.originalEvent;
+//        $(this).scrollTop(scrollStartPos - e.touches[0].pageY);
+//        e.preventDefault();
+    });
+
 })();
