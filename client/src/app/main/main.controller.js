acf83cdd99bb992e09dbf77a270823cb16b5e464
Joe Oldenburg <joseph.oldenburg@gmail.com>
2015-12-13 17:08:13 -0700

    factoring data into a json file

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -32,391 +32,41 @@
   /** @ngInject */
   function MainController($scope, $http, $timeout, webDevTec, toastr) {
 
-
-    $scope.preLoadedSpheres = 
-    [
-        {
-            "src":"/assets/images/cryosphere.png",
-            "sphereName": "Cryosphere"
-        } ,
-        {
-            "src":"/assets/images/atmosphere.png",
-            "sphereName": "Atmosphere"
-        } ,
-        {
-            "src":"/assets/images/forests.png",
-            "sphereName": "Forest"
-        } , 
-        {
-            "src":"/assets/images/pollution.png",
-            "sphereName": "Pollution"
-        } , 
-        {
-            "src":"/assets/images/agriculture.png",
-            "sphereName": "Agriculture"
-        } , 
-        {
-            "src":"/assets/images/education.png",
-            "sphereName": "Education"
-        } , 
-        {
-            "src":"/assets/images/built-env.png",
-            "sphereName": "Built Environment"
-        } , 
-        {
-            "src":"/assets/images/energy.png",
-            "sphereName": "Energy"
-        } ,
-        {
-            "src":"/assets/images/oceans.png",
-            "sphereName": "Hydrosphere"
-        } 
-    ]
-
-    var feeds = 
-    [
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.geology",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.climate",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.energy",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.government",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.landscape",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.math",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.oceans",
-
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.architecture",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.bioinformatics",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.education",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.educationpolicy",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.electricvehicles",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.localfood",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.architecture",
-
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.automotive",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.biodiversity",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.buddhism",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.careers",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.chefs",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.culture",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.dairy",
-
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.environment",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.farming",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.gardening",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.greenhome",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.herbal",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.immigration",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.jazz",
-
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.oil",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.organicgardening",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.progressive",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.rainforest",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.science",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.startups",
-        "http://v3.api.r88r.net/v3/headlines?cname=V3A.transportation"
-    ]
-
     var currentSphereIndex = 1;
     var nextSphereUp = 2;
     var nextSphereDown; // = $scope.preLoadedSpheres[$scope.preLoadedSpheres.length - 1];
     $scope.allSpheresVisible = false;
-
-
-
-    //This is jQuery, try not to mix angular and jquery when possible, because
-    // they both do DOM manipulation.
-    $(".sphere-top-img-static").attr("src", $scope.preLoadedSpheres[currentSphereIndex + 1].src);
-    $(".sphere-middle-img-static").attr("src", $scope.preLoadedSpheres[currentSphereIndex].src);
-    $(".sphere-bottom-img-static").attr("src", $scope.preLoadedSpheres[currentSphereIndex - 1].src);
-    //$(".sphere-bottom-img-static").attr("src", $scope.preLoadedSpheres[$scope.preLoadedSpheres.length - 1].src);
-
     $scope.optionsNowVisible = false;
-
-    //Couldn't get Angular to update the dom when switching spheres.  So frustrating.
-    //$scope.breadcrumbSphere = $scope.preLoadedSpheres[currentSphereIndex].sphereName;
-    $("#breadcrumpSphereName").text($scope.preLoadedSpheres[currentSphereIndex].sphereName);
-
     $scope.backgroundcolor = 'white';
     $scope.backgroundimage= 'full';
     $scope.visibleStory = "";
     $scope.mySavedStories = [];
     $scope.numberOfRows = 1;
-    $scope.initialR88rResponse = [
-        {
-        headline:
-            "Geology",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.geology",
-        img:{src:
-            "/assets/images/Cool_Geological_Layering.jpg"}
-        },
-        {
-        headline:
-            "Climate",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.climate",
-        img:{src:
-            "/assets/images/cloud-cover-climate.jpg"}
-        },
-        {
-        headline:
-            "Energy",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.energy",
-        img:{src:
-            "/assets/images/energy-feed.jpg"}
-        },
-        {
-        headline:
-            "Government",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.government",
-        img:{src:
-            "/assets/images/capitoldomeban.jpg"}
-        },
-        {
-        headline:
-            "Landscape",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.landscape",
-        img:{src:
-            "/assets/images/1024px-Balaton_Hungary_Landscape.jpg"}
-        },
-        {
-        headline:
-            "Math",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.math",
-        img:{src:
-            "/assets/images/1011math.jpg"}
-        },
-        {
-        headline:
-            "Oceans",    
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.oceans",
-        img:{src:
-            "/assets/images/Atlantic-Ocean.jpg"}
-        },
-        {
-        headline:
-            "Architecture ",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.architecture",
-        img:{src:
-            "/assets/images/lacubo01.jpg"}
-        },
-        {
-        headline:
-            "Bioinformatics",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.bioinformatics",
-        img:{src:
-            "/assets/images/cropped-dna.jpg"}
-        },
-        {
-        headline:
-            "Education",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.education",
-        img:{src:
-            "/assets/images/6357758756001800821152486765_State-Education---Generic-jpg.jpg"}
-        },
-        {
-        headline:
-            "Education Policy",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.educationpolicy",
-        img:{src:
-            "/assets/images/pencilgraphdown_jpg_800x1000_q100.jpg"}
-        },
-        {
-        headline:
-            "Electric Vehicles",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.electricvehicles",
-        img:{src:
-            "/assets/images/electric-car-electric-vechile-EV.jpg"}
-        },
-        {
-        headline:
-            "Local Food",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.localfood",
-        img:{src:
-            "/assets/images/local-food (1).jpg"}
-        },
-        {
-        headline:
-            "Architecture",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.architecture",
-        img:{src:
-            "/assets/images/lacubo01.jpg"}
-        },
-        {
-        headline:
-            "Automotive",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.automotive",
-        img:{src:
-            "/assets/images/sap-erp-automotive.jpg"}
-        },
-        {
-        headline:
-            "Buddhism",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.buddhism",
-        img:{src:
-            "/assets/images/buddhism-feed.jpg"}
-        },
-        {
-        headline:
-            "Careers",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.careers",
-        img:{src:
-            "/assets/images/palyavalasztas.png"}
-        },
-        {
-        headline:
-            "Chefs",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.chefs",
-        img:{src:
-            "/assets/images/pic-12.jpg"}
-        },
-        {
-        headline:
-            "Culture",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.culture",
-        img:{src:
-            "/assets/images/flags_world.jpg"}
-        },
-        {
-        headline:
-            "Dairy",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.dairy",
-        img:{src:
-            "/assets/images/dairy-fat-study-image.jpeg"}
-        },
-        {
-        headline:
-            "Environment",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.environment",
-        img:{src:
-            "/assets/images/environment-feed.jpg"}
-        },
-        {
-        headline:
-            "Farming",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.farming",
-        img:{src:
-            "/assets/images/Farm-Equipment.jpg"}
-        },
-        {
-        headline:
-            "Gardening",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.gardening",
-        img:{src:
-            "/assets/images/gardening-001-640x320.jpg"}
-        },
-        {
-        headline:
-            "Green Home",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.greenhome",
-        img:{src:
-            "/assets/images/green-home-designs-550x388-on-home-designfantastic.jpg"}
-        },
-        {
-        headline:
-            "Herbal",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.herbal",
-        img:{src:
-            "/assets/images/Adrenal_Fatigue_herbal_therapy.png"}
-        },
-        {
-        headline:
-            "Immigration",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.immigration",
-        img:{src:
-            "/assets/images/keep-families-together.jpg"}
-        },
-        {
-        headline:
-            "Jazz",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.jazz",
-        img:{src:
-            "/assets/images/480x374xsatchmo-club-hangover-e1367196787338.jpg.pagespeed.ic.ycQr0BFuvN.jpg"}
-        },
-        {
-        headline:
-            "Oil",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.oil",
-        img:{src:
-            "/assets/images/oil-feed.jpg"}
-        },
-        {
-        headline:
-            "Organic Gardening",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.organicgardening",
-        img:{src:
-            "/assets/images/full-size.jpg"}
-        },
-        {
-        headline:
-            "Biodiversity",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.biodiversity",
-        img:{src:
-            "/assets/images/biodiversity_438x0_scale.jpg"}
-        },
-        {
-        headline:
-            "Progressive",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.progressive",
-        img:{src:
-            "/assets/images/131527463_640.jpg"}
-        },
-        {
-        headline:
-            "Rain Forest",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.rainforest",
-        img:{src:
-            "/assets/images/iStock_000013346231Small(2)_ZqCncpxyzB_l.png"}
-        },
-        {
-        headline:
-            "Startups",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.startups",
-        img:{src:
-            "/assets/images/video.yahoofinance.com@a3be196f-5e8c-32ba-a533-dfb288d4962c_FULL.jpg"}
-        },
-        {
-        headline:
-            "Transportation",
-        url:
-            "http://v3.api.r88r.net/v3/headlines?cname=V3A.transportation",
-        img:{src:
-            "/assets/images/RapidTransit1.png"}
-        }
 
-    ]
+    $http.get('/app/main/mainData.json').
+    then(function(response) {
+            $scope.initialR88rResponse = response.data.r88rData;
+            var feeds = response.data.feeds;
+            $scope.preLoadedSpheres = response.data.preLoadedSpheres;
 
+            //This is jQuery, try not to mix angular and jquery when possible, because
+            // they both do DOM manipulation.
+            //$(".sphere-bottom-img-static").attr("src", $scope.preLoadedSpheres[$scope.preLoadedSpheres.length - 1].src);
+            $(".sphere-top-img-static").attr("src", $scope.preLoadedSpheres[currentSphereIndex + 1].src);
+            $(".sphere-middle-img-static").attr("src", $scope.preLoadedSpheres[currentSphereIndex].src);
+            $(".sphere-bottom-img-static").attr("src", $scope.preLoadedSpheres[currentSphereIndex - 1].src);
+            //Couldn't get Angular to update the dom when switching spheres.  So frustrating.
+            //$scope.breadcrumbSphere = $scope.preLoadedSpheres[currentSphereIndex].sphereName;
+            $("#breadcrumpSphereName").text($scope.preLoadedSpheres[currentSphereIndex].sphereName);
+
+
+            $scope.getSphereFeeds("Atmosphere");
+        }, function(response) {
+            var stop;
+    });
+
+
+    
     $scope.getSphereFeeds = function(sphere){
         switch(sphere){
             case "Cryosphere":
@@ -510,7 +160,6 @@
         }
     }
 
-    $scope.getSphereFeeds("Atmosphere");
 
     $scope.closeSplashScreen = function() {
         $("#splashScreen").fadeOut();
@@ -642,54 +291,6 @@
         }
         return result;
     }
-
-    $scope.toggleBackgroundColor = function() {
-        switch($scope.backgroundcolor) {
-            case 'white':
-                $scope.backgroundcolor = 'gray';
-                break;
-            case 'gray':
-                $scope.backgroundcolor = 'black';
-                break;
-            case 'black':
-                $scope.backgroundcolor = 'white';
-                break;
-        }
-    };//End of the Background Color Toggle
-
-    $scope.toggleBackgroundOpacity = function() {
-        switch($scope.backgroundimage) {
-            case 'clear':
-                $(".sphere-middle-img-static").css("opacity","1");
-                $scope.backgroundimage = 'full';
-                break;
-            case 'full':
-                $(".sphere-middle-img-static").css("opacity",".5");;
-                $scope.backgroundimage = 'half';
-                break;
-            case 'half':
-                $(".sphere-middle-img-static").css("opacity","0");
-                $scope.backgroundimage = 'clear';
-                break;
-        }
-        
-
-    };//End of the Background Image Toggle
-
-    $scope.toggleNumberOfRows = function() {
-        switch($scope.numberOfRows) {
-            case 1:
-                $scope.numberOfRows = 2;
-                break;
-            case 2:
-                $scope.numberOfRows = 3;
-                break;
-            case 3:
-                $scope.numberOfRows = 1;
-                break;
-        }        
-
-    };
 
     $scope.newColumn = function(index, str) {
         if ( index % $scope.numberOfRows == 0 ) { 
